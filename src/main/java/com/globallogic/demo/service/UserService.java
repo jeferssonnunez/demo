b29858471ea8490043259900a3c9d3da01ee130e
Jefersson Nava <jeferssonnunez@gmail.com>
2023-12-17 09:28:02 -0500

    feat: Add user

---
+++ src/main/java/com/globallogic/demo/service/UserService.java
@@ -0,0 +1,46 @@
+package com.globallogic.demo.service;
+
+import com.globallogic.demo.model.entities.PhoneEntity;
+import com.globallogic.demo.model.entities.UserEntity;
+import com.globallogic.demo.model.dto.request.PhoneRequest;
+import com.globallogic.demo.model.dto.request.UserRequest;
+import com.globallogic.demo.model.dto.response.UserResponse;
+import com.globallogic.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserResponse addUser(UserRequest userRequest){
+        List<PhoneEntity> phones = Optional.ofNullable(userRequest.getPhones()).orElse(Collections.emptyList())
+                .stream().map(PhoneRequest::toPhoneEntity).collect(Collectors.toList());
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setPassword(userRequest.getPassword());
+        userEntity.setPhones(phones);
+        userEntity.setEmail(userRequest.getEmail());
+        userEntity.setName(userRequest.getName());
+
+        return userEntityToDto(userRepository.save(userEntity));
+    }
+
+    private UserResponse userEntityToDto(UserEntity userEntity){
+
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(userEntity.getId());
+        userResponse.setActive(userEntity.getActive());
+        userResponse.setCreated(userEntity.getCreated());
+        userResponse.setLastLogin(userEntity.getLastLogin());
+
+        return userResponse;
+    }
+}
